cc42448fa4cb58615f05d89c3e69646aa61ef69b
karakRohan <karakrohan2005@gmail.com>
2026-02-04 01:20:30 +0530

    add

--- Node JS Code/3_main.js
+++ Node JS Code/3_main.js
@@ -2,6 +2,7 @@
 // constfun = require('./4_es6.mjs');
 
 
+
 // console.log(Searching);
 // console.log(Searching.linearSearch([10, 15, 20, 25, 30], 15)); // Output: 1
 // console.log(Searching.binarySearch([10, 15, 20, 25, 30], 25)); // Output: 3
