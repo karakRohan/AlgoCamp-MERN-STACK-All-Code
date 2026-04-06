7b75558b7349e0b7ea953fef3f6ea1786804c7e0
karakRohan <karakrohan2005@gmail.com>
2026-04-07 01:47:20 +0530

    Implement Product class with methods for cart operations and product details

---
+++ 5_22. Advanced Javascript - Prototypes & Objects I Dunder proto/1_Class_Demo.js
@@ -0,0 +1,14 @@
+class Product{
+    addToCart(){
+        console.log("Product added to cart");
+    }
+    removeFromCart(){
+        console.log("Product removed from cart");
+    }
+    displayProduct(){
+        console.log("Displaying product details");
+    }
+    byProduct(){
+        console.log("Buying product");
+    }
+}
