298086169f39dde3f4899e3185c02b9f872da362
karakRohan <karakrohan2005@gmail.com>
2026-08-22 13:13:52 +0530

    add

--- 7_24. Advanced JS — Callbacks I Promises I Async JS internals/Part1/Callbacks&Hof.js
+++ 7_24. Advanced JS — Callbacks I Promises I Async JS internals/Part1/Callbacks&Hof.js
@@ -1,6 +1,6 @@
 const arr = [1,2,3,4,5];
 
-function myMap(arr, fn) {
+function myMap(arr, fn) { // myMap is a higher order function that takes an array and a callback function as arguments
     let result = [];  // Create a new array to store the results
     for(let i = 0; i < arr.length; i++) {
         result.push(fn(arr[i]));
@@ -9,6 +9,6 @@ function myMap(arr, fn) {
     return result;
 }
 
-console.log(myMap(arr, function g(element) {
+console.log(myMap(arr, function g(element) { // g is a callback function that takes an element as an argument
     return element**2;
 }))
